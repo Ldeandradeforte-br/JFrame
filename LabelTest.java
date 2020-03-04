c03af26a4ee769f196fd259cff37167306999e41
Ldeandradeforte-br <60586310+Ldeandradeforte-br@users.noreply.github.com>
2020-03-04 15:32:15 -0400

    Criando Janela de Dialogo

---
+++ LabelTest.java
@@ -0,0 +1,14 @@
+
+package lpiiint3a2020;
+
+//@author Aluno Lucas De Andrade Forte
+
+import javax.swing.JFrame;
+public class LabelTest {
+    public static void main( String[] args ){
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        labelFrame.setSize(260, 180);
+        labelFrame.setVisible( true );
+    }
+}
